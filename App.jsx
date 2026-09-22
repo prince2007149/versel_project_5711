cb517e2f772a7dd0bf2fe48a7e6164e0aba47f69
prince2007149 <prince25cs@student.mes.ac.in>
2026-09-22 13:30:28 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import Greeting from './Greeting';
+
+function App() {
+  return (
+    <>
+    <Greeting/>
+    </>
+  )
+}
+
+export default App;
---
+++ Greeting.jsx
@@ -0,0 +1,15 @@
+function Greeting() {
+    return (
+        <>
+        <h1>Hello World</h1>
+
+        <h2>List of Subject</h2>
+        <li>Java</li>
+        <li>Python</li>
+        <li>Full stack</li>
+        <li>OS</li>
+        </>
+    )
+}
+
+export default Greeting;
